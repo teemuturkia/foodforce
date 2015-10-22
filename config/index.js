fd01d6072fb3953976abb95b4745e66e9963b804
Teemu Turkia <teemu@turkia.fi>
2015-10-22 20:44:21 +0300

    Added .gitignore

---
+++ config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "mongo": {
+    "url": "mongodb://localhost/ruokapaikka"
+  },
+  "restaurants": {
+    "urls": [
+      "salpakeskus.php",
+      "raminkonditoria.php",
+      "amarillokouvola.php",
+      "galerie.php",
+      "gyros.php",
+      "bhanchhaghar.php",
+      "neville.php",
+      "pinocchio.php",
+      "rossokouvola.php"
+    ],
+    "static": [
+      "Angela",
+      "Kiinalainen",
+      "Three Wings"
+    ]
+  }
+}
